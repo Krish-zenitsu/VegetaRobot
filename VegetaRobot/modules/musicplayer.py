637da681e562362097d70907ac30324d6e36048a
DARKAMAN6 <89340128+DARKAMAN6@users.noreply.github.com>
2021-12-08 23:49:48 +0530

    Create musicplayer.py

---
+++ VegetaRobot/modules/musicplayer.py
@@ -0,0 +1,50 @@
+__help__ = """
+*  • Groups and channels Voice Chat Music Play 🎧  *
+
+1) Make bot admin (Group and in channel if use cplay)
+2) Start a voice chat
+3) Try /play [song name] for the first time by an admin
+4) If userbot joined enjoy music, If not add @ALEXAASSISTANT to your group and retry
+
+🏮 here is the basic commands:
+
+» /play (song name/link) - play music on video chat
+» /stream (query/link) - stream the yt live/radio live music
+» /vplay (video name/link) - play video on video chat
+» /vstream - play live video from yt live/m3u8
+» /playlist - show you the playlist
+» /video (query) - download video from youtube
+» /song (query) - download song from youtube
+» /lyric (query) - scrap the song lyric
+» /search (query) - search a youtube video link
+
+» /ping - show the bot ping status
+» /uptime - show the bot uptime status
+» /alive - show the bot alive info (in group)
+ 
+🏮 here is the admin commands:
+
+» /pause - pause the stream
+» /resume - resume the stream
+» /skip - switch to next stream
+» /stop - stop the streaming
+» /vmute - mute the userbot on voice chat
+» /vunmute - unmute the userbot on voice chat
+» /volume 1-200 - adjust the volume of music (userbot must be admin)
+» /reload - reload bot and refresh the admin data
+» /userbotjoin - invite the userbot to join group
+» /userbotleave - order userbot to leave from group
+
+⚡️ Powered by Myself_sanskari_bacha AI
+
+⚡️ Powered by alexa music
+
+* 🔷Assistent🔷 *
+
+@ALEXAASSISTANT
+
+* 🔸Bot🔸 *
+
+@ALEXA_MANAGER_ROBOT
+"""
+__mod_name__ = "Music Player🌸"
